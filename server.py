fc8e96cebb19aa9095d8ca45238f2944e5809cab
Pepe García <pepe@pepegar.com>
2024-01-29 10:23:44 +0100

    code from class

--- server.py
+++ server.py
@@ -1,4 +1,13 @@
 # 0. import flask
+from flask import Flask
+
 # 1. create the flask application
+app = Flask("app")
+
 # 2. create a route
+@app.route("/")
+def index():
+    return "<h1>Hello from Flask</h1>"
+
 # 3. run the application
+app.run()
