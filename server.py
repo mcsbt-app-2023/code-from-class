1a534ce21095f2ea95c23f64156396426377dee3
Pepe García <pepe@pepegar.com>
2024-01-30 14:56:25 +0100

    code from first 26 mins of class

--- server.py
+++ server.py
@@ -7,7 +7,17 @@ app = Flask("app")
 # 2. create a route
 @app.route("/")
 def index():
-    return "<h1>Hello from Flask</h1>"
+    return """
+    <h1>Hello from Flask</h1>
+    <h2><a href="/login">go to login</a></h2>
+    """
+
+@app.route("/login")
+def login():
+    return """
+    <h1>please login!</h1>
+    <h2><a href="/">go back home</a></h2>
+    """
 
 # 3. run the application
 app.run()
