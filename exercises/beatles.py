4ff62149711bf2fd89f0f68f352379ad2ade0746
Pepe García <pepe@pepegar.com>
2024-02-14 11:35:13 +0100

    code from class

--- exercises/beatles.py
+++ exercises/beatles.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+beatles = [
+    "John",
+    "George",
+    "Ringo",
+    "Paul",
+]
+
 
 @app.route("/")
 def index():
-    return render_template("beatles.html")
+    return render_template("beatles.html", band=beatles)
 
 
 app.run(debug=True)
--- exercises/loggedin.py
+++ exercises/loggedin.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    logged_in = True
-    return render_template("loggedin.html")
+    logged_in = False
+    return render_template("loggedin.html", logged_in=logged_in)
 
 
 app.run(debug=True)
