adcd0a03530b94c56ce9b064b974c05204162993
Pepe García <pepe@pepegar.com>
2024-02-26 09:56:03 +0800

    code from class

--- exercises/barker.py
+++ exercises/barker.py
@@ -1,8 +1,20 @@
-from flask import Flask, send_from_directory, render_template
+from flask import (
+    Flask,
+    send_from_directory,
+    render_template,
+    request,
+    session,
+    redirect,
+)
 
 app = Flask(__name__)
 
-users_database = {"churro": "pepepassword", "gnochhi": "gnocchipassword"}
+app.config["SECRET_KEY"] = "something very secret"
+
+users_database = {
+    "churro": "ab5743ce10dcb8eec8eba72e2aae97f684891786",
+    "gnocchi": "0adbe085fd8eb9a6ace9300130d5d3f6ff0baf22",
+}
 
 # hashing values cheatsheet:
 # import a hash function from hashlib module (sha1, for example)
@@ -24,6 +36,18 @@ def about():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
+@app.route("/handle-login", methods=["POST"])
+def handle_login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    if username in users_database and users_database[username] == password:
+        session["username"] = username
+        return redirect("/")
+    else:
+        return redirect("/register")
 
 
 @app.route("/static/style.css")
